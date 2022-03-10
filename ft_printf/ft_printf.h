14bd430248819d6130562d151cf89700df1bc719
sua1223 <70890638+sua1223@users.noreply.github.com>
2022-03-10 20:54:20 +0900

    [ft_printf] solving Mandatory part

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,16 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_putstr(char *str);
+int	ft_putchar(char c);
+int	ft_printf(const char *format, ...);
+int	ft_putnbr(int n);
+int	ft_hexdecimal(unsigned int nbr);
+int	ft_hedecimal(unsigned int nbr);
+int	ft_unsigned(int nbr);
+int	ft_pointer(void *ptr);
+int	ft_pointerhedexcimal(unsigned long nbr);
+#endif
---
+++ ft_printf/ft_printf_utils2.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sujang <sujang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/10 20:26:08 by sujang            #+#    #+#             */
+/*   Updated: 2022/03/10 20:51:00 by sujang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_unsigned(int nbr)
+{
+	int				len;
+	unsigned int	n;
+
+	n = nbr;
+	len = 0;
+	if (n > 9)
+	{
+		len += ft_putnbr(n / 10);
+		len += ft_putchar(n % 10 + '0');
+	}
+	else
+		len += ft_putchar(n + '0');
+	return (len);
+}
+
+int	ft_pointerhexdecimal(unsigned long nbr)
+{
+	int	len;
+
+	len = 0;
+	if (nbr < 16)
+	{
+		if (nbr < 10)
+			len += ft_putchar(nbr + 48);
+		else
+			len += ft_putchar(nbr + 87);
+	}
+	if (nbr > 15)
+	{
+		len += ft_pointerhexdecimal(nbr / 16);
+		len += ft_pointerhexdecimal(nbr % 16);
+	}
+	return (len);
+}
+
+int	ft_pointer(void *ptr)
+{
+	int	len;
+
+	len = 0;
+	len += ft_putstr("0x");
+	len += ft_pointerhexdecimal((unsigned long)ptr);
+	return (len);
+}
